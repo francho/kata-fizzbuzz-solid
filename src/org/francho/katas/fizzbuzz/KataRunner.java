2595d962c3f0d70a14a54cb4717bc40798ca1099
Francho <lab@francho.org>
2013-12-20 22:08:14 +0100

    runner optimization

--- src/org/francho/katas/fizzbuzz/KataRunner.java
+++ src/org/francho/katas/fizzbuzz/KataRunner.java
@@ -2,9 +2,9 @@ package org.francho.katas.fizzbuzz;
 
 public class KataRunner {
     public static void main(String[] args) {
+        final FizzBuzzBuilder builder = new FizzBuzzBuilder(0);
         for(int x=1;x<=100;x++) {
-            FizzBuzzBuilder builder = new FizzBuzzBuilder(x);
-            System.out.println(builder);
+            System.out.println(builder.build(x));
         }
     }
 }
